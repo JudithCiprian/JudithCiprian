6d5ba30c27b6021d0ff38ba81cd8dcb812915d87
JudithCiprian <judith.ciprian.decastro@gmail.com>
2024-05-26 21:56:23 -0400

    Add files via upload

--- tarea2/main.c
+++ tarea2/main.c
@@ -1,26 +1,17 @@
-//ejercicio 2.1
+//ejercicio 2.2
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-//declaramos las variables
-    float PRE, NPR;
-
-//imprime la frase
-    printf("ingreese el precio del producto:");
-
-//lee el valor de la variable.
-    scanf ("%f", &PRE);
-
-//condiciona que la variable PRE sea mayor que 1500
+float PRE, NPR;
+    printf("Ingrese el precio del producto: ");
+    scanf("%f, &PRE");
     if(PRE > 1500)
     {
-//asignan el valor a NPR de la multiplicacion de PRE*1.11
-        NPR= PRE * 1.11;
-//imprime la frase
-        printf("\nNuevo precio: %7.2f", NPR);
-
+        NPR = PRE * 1.11;
+        printf("\nNuevo precio: %7.2f",NPR);
     }
+
     return 0;
 }
