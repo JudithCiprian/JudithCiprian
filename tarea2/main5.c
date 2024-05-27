9d5a6cd18ca88e122d8efe9fa02e6c18c53f9312
JudithCiprian <judith.ciprian.decastro@gmail.com>
2024-05-26 22:45:40 -0400

    Add files via upload

---
+++ tarea2/main5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+    //muestra la estructura de switch en c
+int main()
+switch(<selector>)
+{
+   case <valor1> :<accion1>;
+        break; //es necesario escribir break para no evaluar los otros casos
+   case <valor2> :<accion2>;
+        break;
+   case<valor3>  :<accion3>;
+        break;
+
+    printf("Hello world!\n");
+
+
+
+    return 0;
+}
+accion3+1;
