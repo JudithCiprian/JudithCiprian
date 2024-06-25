49ffc9b071fc9623156e181af1f4698c6e39302b
JudithCiprian <judith.ciprian.decastro@gmail.com>
2024-06-25 08:51:14 -0400

    Add files via upload

---
+++ tarea5/main7.9.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cuenta(char *cad, char car);
+
+int main(void) {
+    char car, cad[50];
+    int res;
+
+    printf("\nIngrese la cadena de caracteres: ");
+    fgets(cad, sizeof(cad), stdin);
+    // Eliminar el salto de línea al final de la cadena
+    cad[strcspn(cad, "\n")] = '\0';
+
+    printf("\nIngrese el caracter: ");
+    car = getchar();
+
+    res = cuenta(cad, car);
+
+    printf("\n\n%c se encuentra %d veces en la cadena %s", car, res, cad);
+
+    return 0;
+}
+
+int cuenta(char *cad, char car) {
+    int i = 0, r = 0;
+    while (cad[i] != '\0') {
+        if (cad[i] == car)
+            r++;
+        i++;
+    }
+    return r;
+}
